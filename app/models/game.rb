db1dd4fd525952e344a598695d42f216593d8fa8
Seth Siegler <usaseth@gmail.com>
2015-11-07 14:20:27 -0800

    completed the first question and answer building

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,20 @@
 class Game < ActiveRecord::Base
   has_many :users
   has_many :questions
+
+  def build_neighborhood_question(vendor)
+    # Create a quesion
+    # get a listing, save it's neigbhorhood and image URL
+    # Find it's neighborhood
+    # Find a few more listings in similar price range to get 3 other neighborhood names
+    # Update  the question to include the image of the listing
+    # Create four answers (3 with 3 decoy names and 1 with the correct name)
+
+    if vendor == "armls"
+      @location = "Phoenix"
+    end
+
+    Question.new.get_first_listing(@location,vendor,self.id)
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,29 +3,19 @@ class Question < ActiveRecord::Base
   belongs_to :game
   has_many :guesses
 
-  def neighborhood_question(vendor)
-    # Create a quesion
-    # get a listing, save it's neigbhorhood and image URL
-    # Find it's neighborhood
-    # Find a few more listings in similar price range to get 3 other neighborhood names
-    # Update  the question to include the image of the listing
-    # Create four answers (3 with 3 decoy names and 1 with the correct name)
-
-    if vendor == "armls"
-      near = "Phoenix"
-    end
-  end
 
   def get_first_listing(location,vendor,game_id)
     # get first listing
     # create a question
     # create an answer
+
+    @location = location
     options = {
       :status => "Active",
       :near => "#{location}",
       :radius => 30
     }
-    response = HTTParty.get("https://rets.io/api/v1/#{vendor}/listings?access_token=2a1f5ffae087869723cc83e87be7ab63", query:options)
+    response = HTTParty.get("https://rets.io/api/v1/#{vendor}/listings?access_token=#{ENV['server_token']}", query:options)
     response = response.to_hash['bundle']
     count = response.count
     number_choice = rand(count -1)
@@ -33,8 +23,10 @@ class Question < ActiveRecord::Base
     @hood_name = chosen_listing['subdivision']
     @chosen_mls_num = chosen_listing['mlsListingID']
     @image_url = chosen_listing['media'].first['url']
+    @price = chosen_listing['price']
 
     create_neighborhood_question(@image_url,game_id)
+
   end
 
   def create_neighborhood_question(image,game_id)
@@ -42,20 +34,49 @@ class Question < ActiveRecord::Base
           body:"What neighborhood is this listing in?",
           image_url:image)
     create_correct_answer(@hood_name,question.id)
-    get_other_listings
+    get_other_listings(@price, @hood_name, question.id)
   end
 
   def create_correct_answer(hood_name,question_id)
-    Answer.create!(correct:true, body:@hood_name)
+    Answer.create!(correct:true, body:@hood_name, question_id:question_id)
   end
 
-  def get_other_listings()
+  def get_other_listings(price, subdivision, question_id)
+    low_price = (price - (price*0.1))
+    high_price = (price + (price*0.1))
 
-    create_wrong_answers(question_id)
+    options = {
+      :status => "Active",
+      :near => "#{@location}",
+      :radius => 30,
+    }
+
+    response = HTTParty.get("https://rets.io/api/v1/armls/listings?access_token=#{ENV['server_token']}&subdivision[ne]=#{@hood_name}&price[lt]=#{high_price}&price[gt]=#{low_price}", query:options)
+    response = response.to_hash['bundle']
+    count = response.count
+    first_listing = response[0]
+    choice_1 = first_listing['subdivision']
+
+                                                                                                                    # &and.0.zoning.ne=C-1&and.1.zoning.ne=PUD
+    response = HTTParty.get("https://rets.io/api/v1/armls/listings?access_token=#{ENV['server_token']}&and.0.subdivision.ne=#{@hood_name}&and.1.subdivision.ne=#{choice_1}&price[lt]=#{high_price}&price[gt]=#{low_price}", query:options)
+    response = response.to_hash['bundle']
+    count = response.count
+    second_listing = response[0]
+    choice_2 = second_listing['subdivision']
+
+    response = HTTParty.get("https://rets.io/api/v1/armls/listings?access_token=#{ENV['server_token']}&and.0.subdivision.ne=#{@hood_name}&and.1.subdivision.ne=#{choice_1}&and.2.subdivision.ne=#{choice_2}&price[lt]=#{high_price}&price[gt]=#{low_price}", query:options)
+    response = response.to_hash['bundle']
+    count = response.count
+    third_listing = response[0]
+    choice_3 = third_listing['subdivision']
+
+    create_wrong_answers(question_id, choice_1, choice_2, choice_3)
   end
 
-  def create_wrong_answers(question_id)
-
+  def create_wrong_answers(question_id, choice_1, choice_2, choice_3)
+    Answer.create!(correct:false, body:choice_1, question_id:question_id)
+    Answer.create!(correct:false, body:choice_2, question_id:question_id)
+    Answer.create!(correct:false, body:choice_3, question_id:question_id)
   end
 
 end
